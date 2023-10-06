5331646d4e0e337e8285ec3bc72fbbd2edd1b6f4
MiraiTsukiGames <you@example.com>
2023-10-06 13:37:46 +0200

    up

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -33,7 +33,7 @@ const Education = () => {
           <div className="flex items-center justify-center">
             <div className="flex space-x-2">
               <a
-                href="./src/assets/Attestato Completamento Master.pdf"
+                href="../assets/Attestato Completamento Master.pdf"
                 download
               >
                 <button className="neno-button shadow-xl hover:shadow-cyan-800/50 border-2  hover:text-cyan-500 hover:bg-slate-900 border-cyan-800 rounded-lg py-4 px-8 uppercase relative overflow-hidden">
