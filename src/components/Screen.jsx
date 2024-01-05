322bda257218ccb4be7d5a63b7ca38af235ca826
Sassa1993 <118011618+Sassa19932@users.noreply.github.com>
2024-01-05 20:29:58 +0100

    aggiornamento

--- src/components/Screen.jsx
+++ src/components/Screen.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Screen ({children}) {
   return (
-    <div className='b_glow border-2 border-[#ff19f3] rounded-3xl mx-auto lg:w-[50rem] mt-5 bg-slate-900'>
+    <div className='b_glow border-2 border-[#ff19f3] rounded-3xl mx-auto lg:w-[50rem] mt-5 hologram'>
         {children}
     </div>
   )
--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -25,7 +25,7 @@ const Skills = () => {
         <div className="grid lg:grid-cols-2 grid-cols-1 gap-5 mt-5 justify-around h-[30rem] overflow-y-auto lg:overflow-y-hidden lg:h-full">
           <div
             data-aos="fade-up"
-            className="flex items-center justify-center font-semibold text-red-600 rounded-ss-3xl rounded-br-3xl h-[7rem] w-[9rem] border-2 border-[#ff19f3] b_glow bg-slate-900"
+            className="flex items-center justify-center font-semibold text-red-600 rounded-ss-3xl rounded-br-3xl h-[7rem] w-[9rem] border-2 border-[#ff19f3] hologram"
           >
             <a
               href="https://www.w3schools.com/html/"
@@ -37,7 +37,7 @@ const Skills = () => {
           </div>
           <div
             data-aos="fade-down"
-            className="flex items-center justify-center font-semibold text-blue-700 rounded-ss-3xl rounded-br-3xl h-[7rem] w-[9rem] border-2 border-[#ff19f3] b_glow"
+            className="flex items-center justify-center font-semibold text-blue-700 rounded-ss-3xl rounded-br-3xl h-[7rem] w-[9rem] border-2 border-[#ff19f3] hologram"
           >
             <a
               href="https://www.w3schools.com/css/default.asp"
@@ -49,7 +49,7 @@ const Skills = () => {
           </div>
           <div
             data-aos="fade-down"
-            className="flex items-center justify-center font-semibold text-yellow-500 rounded-ss-3xl rounded-br-3xl h-[7rem] w-[9rem] border-2 border-[#ff19f3] b_glow"
+            className="flex items-center justify-center font-semibold text-yellow-500 rounded-ss-3xl rounded-br-3xl h-[7rem] w-[9rem] border-2 border-[#ff19f3] hologram"
           >
             <a href="https://javascript.info/" target="_blank" rel="noreferrer">
               <DiJavascript size={50} />
@@ -57,7 +57,7 @@ const Skills = () => {
           </div>
           <div
             data-aos="fade-down"
-            className="flex items-center justify-center font-semibold text-cyan-500 rounded-ss-3xl rounded-br-3xl h-[7rem] w-[9rem] border-2 border-[#ff19f3] b_glow"
+            className="flex items-center justify-center font-semibold text-cyan-500 rounded-ss-3xl rounded-br-3xl h-[7rem] w-[9rem] border-2 border-[#ff19f3] hologram"
           >
             <a href="https://react.dev/" target="_blank" rel="noreferrer">
               <FaReact size={50} />
@@ -65,14 +65,14 @@ const Skills = () => {
           </div>
           <div
             data-aos="fade-up"
-            className="flex items-center justify-center font-semibold text-blue-700 rounded-ss-3xl rounded-br-3xl h-[7rem] w-[9rem] border-2 border-[#ff19f3] b_glow"
+            className="flex items-center justify-center font-semibold text-blue-700 rounded-ss-3xl rounded-br-3xl h-[7rem] w-[9rem] border-2 border-[#ff19f3] hologram"
           >
             <a href="https://www.typescriptlang.org/" target="_blank" rel="noreferrer">
               <BiLogoTypescript size={50} />
             </a>
           </div>
           <div
-            className="flex items-center justify-center font-semibold text-red-600 rounded-ss-3xl rounded-br-3xl h-[7rem] w-[9rem] border-2 border-[#ff19f3] b_glow"
+            className="flex items-center justify-center font-semibold text-red-600 rounded-ss-3xl rounded-br-3xl h-[7rem] w-[9rem] border-2 border-[#ff19f3] hologram"
           >
             <a href="https://angular.io" target="_blank" rel="noreferrer">
               <FaAngular size={50} />
